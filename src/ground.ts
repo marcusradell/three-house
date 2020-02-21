705ccb3f8a02a15ba23c7285322b2ed0f3d08542
Marcus Rådell <marcus@radell.net>
2020-02-21 23:36:29 +0100

    change ground to a cylinder

--- src/ground.ts
+++ src/ground.ts
@@ -1,15 +1,16 @@
-import { CircleGeometry, Mesh, MeshPhongMaterial as Material } from "three";
+import { CylinderGeometry, Mesh, MeshPhongMaterial as Material } from "three";
 
 export type Ground = {
-  geo: CircleGeometry;
+  geo: CylinderGeometry;
   mesh: Mesh;
 };
 
 type CreateGround = (quality: number) => Ground;
 
 export const createGround: CreateGround = quality => {
-  const geo = new CircleGeometry(200, 1000 * quality);
-  geo.rotateX(-Math.PI / 2);
+  const height = 10;
+  const geo = new CylinderGeometry(190, 200, height, 1000 * quality);
+  geo.translate(0, -height / 2, 0);
   const mat = new Material({ color: 0x33bb66 });
   const mesh = new Mesh(geo, mat);
   mesh.receiveShadow = true;
